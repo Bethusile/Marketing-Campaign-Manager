d331f8534afd9d0301d203ff0a44bf701c7b8860
Jayson Bam <u23557894@tuks.co.za>
2025-12-05 13:01:22 +0200

    removed comments

--- server/src/route/index.ts
+++ server/src/route/index.ts
@@ -2,9 +2,4 @@ import { Router } from 'express';
 
 const router = Router();
 
-// Example route
-router.get('/', (req, res) => {
-  res.json({ message: 'API is working!' });
-});
-
 export default router;
