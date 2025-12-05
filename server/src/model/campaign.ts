653cb45bef9f4610cefd4e4ba594fd6c7caabefb
Jayson Bam <u23557894@tuks.co.za>
2025-12-05 08:19:19 +0200

    Database connect with, migration

---
+++ server/src/model/campaign.ts
@@ -0,0 +1,56 @@
+import { DataTypes, Model } from 'sequelize';
+import { sequelize } from '../db/connect';
+
+class Campaign extends Model {}
+
+Campaign.init({
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  title: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    defaultValue: '',
+  },
+  message: {
+    type: DataTypes.TEXT,
+    allowNull: false,
+    defaultValue: '',
+  },
+  target_url: {
+    type: DataTypes.STRING(255),
+    allowNull: false,
+    defaultValue: '',
+  },
+  overlay_url: {
+    type: DataTypes.STRING(255),
+    allowNull: false,
+    defaultValue: '',
+  },
+  button_url: {
+    type: DataTypes.STRING(255),
+    allowNull: false,
+    defaultValue: '',
+  },
+  comments: {
+    type: DataTypes.TEXT, 
+    allowNull: true,
+  },
+  createdAt: {
+    type: DataTypes.DATE,
+    allowNull: true,
+  },
+  updatedAt: {
+    type: DataTypes.DATE,
+    allowNull: true,
+  },
+}, {
+  sequelize,
+  modelName: 'Campaign',
+  tableName: 'campaigns',
+  timestamps: true, // Enable automatic timestamps
+});
+
+export default Campaign;
