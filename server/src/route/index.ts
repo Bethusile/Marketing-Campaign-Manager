885fa7194ae68c9e515932c44510c3223f88f9dc
Bethusile <bethusilejm@gmail.com>
2025-12-11 11:43:36 +0200

    removed all controllers

--- server/src/route/index.ts
+++ server/src/route/index.ts
@@ -1,49 +1,18 @@
-
 import { Router } from 'express';
 import { allowAll } from '../middleware/allowAll';
-import {
-  getAllCampaigns,
-  getActiveCampaigns,
-  getCampaignById,
-  postCampaign,
-  updateCampaign,
-  deleteCampaign,
-  upload
-} from '../controller/campaignController';
+
 import { toggleCampaignActive } from '../handlers/campaignStatusHandler';
-import { getTarget } from '../controller/targetController';
 import { uploadCampaignHandler } from '../handlers/uploadCampaign';
 import { getImagesHandler } from '../handlers/getImagesHandler';
 import { getCampaign, getUnredactedImage } from '../handlers/ar';
 import { getRedactedImagesBulk, updateTarget } from '../handlers/postTarget';
-import { upload as fileUpload } from '../utils/fileManager';
+import { upload } from '../utils/fileManager';
 import { uploadImagesHandler } from '../handlers/UploadImages';
 
 const router = Router();
 
-// Campaign routes
-router.get('/getCampaign/all', allowAll, getAllCampaigns);
-router.get('/getCampaign/active', allowAll, getActiveCampaigns);
-router.get('/getCampaign/:id', allowAll, getCampaignById);
-router.post(
-  '/postCampaign',
-  allowAll,
-  upload.fields([
-    { name: 'overlay', maxCount: 1 },
-    { name: 'target', maxCount: 1 },
-  ]),
-  (req, res) => { void postCampaign(req, res); }
-);
-router.put(
-  '/updateCampaign/:id',
-  allowAll,
-  upload.fields([
-    { name: 'overlay', maxCount: 1 },
-    { name: 'target', maxCount: 1 },
-  ]),
-  (req, res) => { void updateCampaign(req, res); }
-);
-router.delete('/deleteCampaign/:id', allowAll, deleteCampaign);
+// Campaign routes - del
+
 
 router.post(
   '/campaign/upload',
@@ -51,8 +20,8 @@ router.post(
   uploadCampaignHandler,
 );
 
-// Target route
-router.get('/getTarget', allowAll, getTarget);
+// Target route - del
+
 // Toggle Campaign Active route
 router.put('/toggleCampaign/:id', allowAll, toggleCampaignActive);
 
@@ -65,7 +34,7 @@ router.post('/marketing/images/redacted', allowAll, (req, res) => { void getReda
 router.put(
   '/marketing/campaign/target',
   allowAll,
-  fileUpload.fields([{ name: 'targetMind', maxCount: 1 }]),
+  upload.fields([{ name: 'targetMind', maxCount: 1 }]),
   (req, res) => { void updateTarget(req, res); }
 );
 
