55df53f344a931fad53bac6c09452a9c7981e30a
Jayson Bam <u23557894@tuks.co.za>
2025-12-11 08:59:03 +0200

    add Target to Manager

--- campaign_manager/src/main.ts
+++ campaign_manager/src/main.ts
@@ -1,5 +1,8 @@
 import  './style.css';
 import { uploadImagesHelper } from './helpers/uploadImagesHelper';
+import { addTargetHelper } from './helpers/addTarget';
+import './vendor/aframe.min.js'
+import './vendor/mindar-image-aframe.prod.js'
 
 type JSONValue = string | number | boolean | null | JSONValue[] | { [key: string]: JSONValue };
 
@@ -54,6 +57,10 @@ window.setResp = setResp;
 //upload images section
 const uploadForm = uploadImagesHelper(window.setResp!);
 app.appendChild(uploadForm);
+
+// add target (compile & upload) form
+const addTargetForm = addTargetHelper(window.setResp!);
+app.appendChild(addTargetForm);
 
 app.appendChild(respSection);
 
