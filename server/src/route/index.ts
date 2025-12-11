7b59609175ed88ea46797de4d40f3b0b7e83f536
Jayson Bam <u23557894@tuks.co.za>
2025-12-11 07:27:18 +0200

    Added API endpoint for target handelding

--- server/src/route/index.ts
+++ server/src/route/index.ts
@@ -11,6 +11,9 @@ import {
   upload
 } from '../controller/campaignController';
 import { getTarget } from '../controller/targetController';
+import { getCampaign, getUnredactedImage } from '../handlers/ar';
+import { getRedactedImagesBulk, updateTarget } from '../handlers/postTarget';
+import { upload as fileUpload } from '../utils/fileManager';
 
 const router = Router();
 
@@ -40,5 +43,19 @@ router.delete('/deleteCampaign/:id', allowAll, deleteCampaign);
 
 // Target route
 router.get('/getTarget', allowAll, getTarget);
+
+// AR routes
+router.get('/ar/:id', allowAll, (req, res) => { void getCampaign(req, res); });
+router.get('/ar/image/unredacted/:id', allowAll, (req, res) => { void getUnredactedImage(req, res); });
+
+// Post target routes
+router.post('/marketing/images/redacted', allowAll, (req, res) => { void getRedactedImagesBulk(req, res); });
+router.put(
+  '/marketing/campaign/target',
+  allowAll,
+  fileUpload.fields([{ name: 'targetMind', maxCount: 1 }]),
+  (req, res) => { void updateTarget(req, res); }
+);
+
 
 export default router;
